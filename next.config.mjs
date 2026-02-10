85e061dc2b6210285f037e9f2eef2fae3800b518
MohaDicko <mohadicko95@gmail.com>
2026-02-10 19:11:03 +0000

    feat: Configure Next.js to ignore TypeScript and ESLint build errors, update lint script, and add Prisma generate postinstall.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // typescript: {
-  //   ignoreBuildErrors: false,
-  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
   },
